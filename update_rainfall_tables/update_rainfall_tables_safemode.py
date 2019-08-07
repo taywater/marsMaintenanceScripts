654d25c13f9cc6a1ed1b4fc00883d0192be14260
Taylor <Taylor.Heffernan@Phila.Gov>
2019-08-07 16:09:16 -0400

    Edited the Rainfall maintenance script to target the H&H Modeling CentralDB instead of the old CSO Model Rain access DB.

--- update_rainfall_tables/update_rainfall_tables_safemode.py
+++ update_rainfall_tables/update_rainfall_tables_safemode.py
@@ -11,40 +11,11 @@ else:
 #However, when specifying file paths that will be read by the R command, we must type \\\\
 #Because specifying \\ in the Python script will cause R to see \, and it will choke on the singleton \ just like Python would
 
-###Section 1: Grab and unzip the rain gage database
-###We'll be copying and unzipping the H&H database 
-#Where are the databases located?
-hhdb = "\\\\pwdoows\\OOWS\\Modeling\\Data\\H&H Databases\\PWD Raingauge\\CSORain2010.zip"
-localdir = os.path.expanduser("~") + "\\Documents"
-localzip = localdir + "\\" + "CSORain2010.zip"
-
-#Copy the H&H DB to our home directory
-print("Copying H&H rainfall DB to local directory")
-shutil.copyfile(hhdb, localzip)
-
-#Unzip the zipped file
-#Note: extractall() is depricated in Python 3. We're using Python 2 because that's what's in ArcGIS
-#Note: This filepath is used by Python only, and thus needs \\ as a separator
-print("Unzipping rainfall DB")
-localzipobject = zipfile.ZipFile(localzip, 'r')
-localzipobject.extractall(localdir)
-
-#After unzipping, the db is here
-localdb = os.path.expanduser("~") + "\\Documents\\CSORain2010\\CSORain2010.mdb"
-
-#We need the \\ in localdb to be \\\\ to pass it to R
-#We have to do this in a second step because os.path.expanduser(~) will always return \\
-#We will use a regular expression to turn \\ into \\\\. The regular expression to identify a literal \\ is \\\\
-#This is very dumb
-localdb = re.sub('\\\\', '\\\\\\\\', localdb)
-print(localdb)
-
-
 #In order to send the GIS output file to the R script, we need to replicate the file naming mechanism from the arcpy script.
 current_date = datetime.datetime.now()
 datestring = current_date.strftime("%Y%m%dT%H%M")
 
-###Section 3: Run the R script that will generate the updates for the database
+###Section 1: Run the R script that will generate the updates for the database
 #We'll be calling this R script from within this python script
 #We'll be composing a string that will be sent to the command line via a subprocess
 
@@ -61,7 +32,7 @@ output_file = "'output\\\\" + datestring + "_update_rainfall_tables.html" + "'"
 r_exe = "Rscript.exe"
 
 #Compose the R command that will be passed to r_exe
-r_command = "rmarkdown::render(" + r_script + ", params = list(gagedb='" + localdb + "', database=" + database + ", write=" + writeflag + ")" + ", output_file =" + output_file + ")"
+r_command = "rmarkdown::render(" + r_script + ", params = list(database=" + database + ", write=" + writeflag + ")" + ", output_file =" + output_file + ")"
 
 print(r_command)
 
