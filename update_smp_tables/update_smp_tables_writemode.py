0b34e61726acf157e9d07144c661f10257b07286
taywater <50152407+taywater@users.noreply.github.com>
2021-02-03 07:43:00 -0500

    Major overhaul for sf package

--- update_smp_tables/update_smp_tables_writemode.py
+++ update_smp_tables/update_smp_tables_writemode.py
@@ -3,7 +3,7 @@ import datetime, os, subprocess, webbrowser, re
 
 #Since it's not going to be run in interactive mode, we need to load PYTHONSTARTUP 
 if os.path.isfile(os.environ['PYTHONSTARTUP']):
-  execfile(os.environ['PYTHONSTARTUP'])
+  exec(open(os.environ['PYTHONSTARTUP']).read())
 else:
   sys.exit("You don't have a .pythonrc file in your PYTHONSTARTUP environment variable.")
 
@@ -11,43 +11,13 @@ else:
 #However, when specifying file paths that will be read by the R command, we must type \\\\
 #Because specifying \\ in the Python script will cause R to see \, and it will choke on the singleton \ just like Python would
 
-###Section 1: Generate GIS data file
-#Run some arcpy commands that export up-to-date SMP centroids from DataConv
-#This script generates an ESRI shapefile containing SMP centroids in the following folder:
-# A:\Scripts\Maintenance\update_smp_tables\centroids_folder
-#Note: execfile() is depricated in Python 3. We're using Python 2 because that's what's in ArcGIS
-#Note: This filepath is used by Python only, and thus needs \\ as a separator
-execfile(MAINTENANCEFOLDER + "\\update_smp_tables\\arcpy_centroids_export.py")
-
-#In order to send the GIS output file to the R script, we need to replicate the file naming mechanism from the arcpy script.
-#We must round the datestring because it's possible for the arcpy script to take more than 60 seconds to execute
-#In which case getting the current datetime at a minute-scale resolution won't get the same number as it did in that script
-current_date = datetime.datetime.now()
-
-def roundTime(dt=None, roundTo=60):
-   """Round a datetime object to any time lapse in seconds
-   dt : datetime.datetime object, default now.
-   roundTo : Closest number of seconds to round to, default 1 minute.
-   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
-   """
-   if dt == None : dt = datetime.datetime.now()
-   seconds = (dt.replace(tzinfo=None) - dt.min).seconds
-   rounding = (seconds+roundTo/2) // roundTo * roundTo
-   return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
-
-rounddate = roundTime(current_date, roundTo = 60 * 15) #Round to the nearest 15 minutes
-datestring = rounddate.strftime("%Y%m%dT%H%M")
-
-#Note: This filepath is echoed by Python and interpreted by R, so we need \\\\ as a separator
-destinationfolder = re.sub('\\\\', '\\\\\\\\',MAINTENANCEFOLDER) + "\\\\update_smp_tables\\\\centroids_folder"
-
-#The is string is wrapped in single quotes because the resultant R command expects it to be a string literal
-#Note: This filepath is echoed by Python and interpreted by R, so we need \\\\ as a separator
-finalshapefile = "'" + destinationfolder + "\\\\centroids_dem_" + datestring + ".shp" + "'"
-
-###Section 2: Run the R script that will generate the updates for the database
+###Section 1: Run the R script that will generate the updates for the database
 #We'll be calling this R script from within this python script
 #We'll be composing a string that will be sent to the command line via a subprocess
+
+#Date string for filenames.
+current_date = datetime.datetime.now()
+datestring = current_date.strftime("%Y%m%dT%H%M")
 
 #The R script that we'll be executing has runtime parameters that we will be setting in this script
 #Note: r_script, database, password, and output_file are wrapped in single quotes because the resultant R command expects them to be string literals
@@ -62,9 +32,9 @@ output_file = "'output\\\\" + datestring + "_update_smp_tables.html" + "'"
 r_exe = "Rscript.exe"
 
 #Compose the R command that will be passed to r_exe
-r_command = "rmarkdown::render(" + r_script + ", params = list(file=" + finalshapefile + ", database=" + database + ", write=" + writeflag + ")" + ", output_file =" + output_file + ")"
+r_command = "rmarkdown::render(" + r_script + ", params = list(database=" + database + ", write=" + writeflag + ")" + ", output_file =" + output_file + ")"
 
-print r_command
+print (r_command)
 
 #Invoke the R commands to run the script and knit the output
 # -e means Rscript.exe is getting the commands from stdin and not from a file
