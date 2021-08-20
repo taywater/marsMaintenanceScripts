10632e87a67b942109f8d1c0f1a673b7bcdfe345
Nick <nmanna@akrf.com>
2021-08-20 12:21:05 -0400

    new functions; updated constraints; new lookup tables

--- Migration/pg12_new_constraints_20210819.sql
+++ Migration/pg12_new_constraints_20210819.sql
@@ -1,132 +1,139 @@
-pg12_new_constraints_20210819
+--pg12_new_constraints_20210819
 
 #20210819
 #added and then dropped these constraints whoops!
+-- alter table data.ow_leveldata_raw
+-- add constraint ow_leveldata_ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table data.gw_depthdata_raw
+-- add constraint gw_depthdata_ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table fieldwork.well_measurements
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.draindown
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.infiltration
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.overtopping
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.percentstorage
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.snapshot
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- alter table metrics.snapshot_metadata
+-- add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+
+-- ------
+-- alter table data.ow_leveldata_raw
+-- drop constraint ow_leveldata_ow_validity
+
+-- alter table data.gw_depthdata_raw
+-- drop constraint gw_depthdata_ow_validity
+
+-- alter table fieldwork.well_measurements
+-- drop constraint ow_validity
+
+-- alter table metrics.draindown
+-- drop constraint ow_validity
+
+-- alter table metrics.infiltration
+-- drop constraint ow_validity
+
+-- alter table metrics.overtopping
+-- drop constraint ow_validity
+
+-- alter table metrics.percentstorage
+-- drop constraint ow_validity
+
+-- alter table metrics.snapshot
+-- drop constraint ow_validity
+
+-- alter table metrics.snapshot_metadata
+-- drop constraint ow_validity
+
+
+---trying again, but with ow_uid_fkey
 alter table data.ow_leveldata_raw
-add constraint ow_leveldata_ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
+add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
+        REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE no action;
 
 alter table data.gw_depthdata_raw
-add constraint gw_depthdata_ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table fieldwork.well_measurements
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.draindown
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.infiltration
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.overtopping
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.percentstorage
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.snapshot
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-alter table metrics.snapshot_metadata
-add constraint ow_validity CHECK (fieldwork.ow_exists(ow_uid) = true) NOT VALID;
-
-------
-alter table data.ow_leveldata_raw
-drop constraint ow_leveldata_ow_validity
-
-alter table data.gw_depthdata_raw
-drop constraint gw_depthdata_ow_validity
-
-alter table fieldwork.well_measurements
-drop constraint ow_validity
-
-alter table metrics.draindown
-drop constraint ow_validity
-
-alter table metrics.infiltration
-drop constraint ow_validity
-
-alter table metrics.overtopping
-drop constraint ow_validity
-
-alter table metrics.percentstorage
-drop constraint ow_validity
-
-alter table metrics.snapshot
-drop constraint ow_validity
-
-alter table metrics.snapshot_metadata
-drop constraint ow_validity
-
-
----trying again 
-alter table data.ow_leveldata_raw
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
-
-alter table data.gw_depthdata_raw
-add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
-        REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table fieldwork.well_measurements
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.draindown
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.infiltration
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.overtopping
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.percentstorage
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.snapshot
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table metrics.snapshot_metadata
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table fieldwork.deployment
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table fieldwork.future_deployment
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
 
 alter table admin.accessdb
 add constraint ow_uid_fkey FOREIGN KEY (ow_uid)
         REFERENCES fieldwork.ow (ow_uid) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT;
+        ON DELETE no action;
+
+
+----
+
+-- add constraint for research lookup uid to fieldwork.deployment 
+
+-- add site name constraints fkeys to fieldwork that has those? 
--- Migration/pg12_new_functions_20210819.sql
+++ Migration/pg12_new_functions_20210819.sql
@@ -1,1 +1,59 @@
 pg12_new_functions_20210819
+
+#20210819
+#ow_exists
+#DROPPED THIS WHEN I REMEMBERED I COULD FKEY
+-- CREATE OR REPLACE FUNCTION fieldwork.ow_exists(
+-- 	thisow integer)
+--     RETURNS boolean
+--     LANGUAGE 'sql'
+
+--     COST 100
+--     VOLATILE 
+-- AS $BODY$SELECT EXISTS (SELECT TRUE FROM fieldwork.ow WHERE ow_uid = thisow)
+
+-- $BODY$;
+
+-- drop function fieldwork.ow_exists
+
+#20210820
+#event depth functions
+CREATE OR REPLACE FUNCTION metrics.event_depth_bin(
+	eventdepth_in numeric)
+    RETURNS integer
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
+SELECT 
+CASE
+WHEN eventdepth_in < 0.5 THEN 1
+WHEN eventdepth_in < 1 THEN 2
+WHEN eventdepth_in < 1.5 THEN 3 
+WHEN eventdepth_in < 2 THEN 4
+WHEN eventdepth_in < 3 THEN 5
+ELSE 6
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION metrics.event_depth_bin_relative(
+	eventdepth_in numeric,
+	designdepth_in numeric)
+    RETURNS integer
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
+SELECT 
+CASE
+WHEN eventdepth_in - designdepth_in > -0.25 AND eventdepth_in - designdepth_in < 0.25 THEN 3
+WHEN eventdepth_in - designdepth_in >= 0.25 AND eventdepth_in - designdepth_in < 0.75 THEN 4
+WHEN eventdepth_in - designdepth_in >= 0.75 AND eventdepth_in - designdepth_in < 1.25 THEN  5
+WHEN eventdepth_in - designdepth_in <= -0.25 AND eventdepth_in - designdepth_in > -0.75 THEN 2
+WHEN eventdepth_in - designdepth_in <= -0.75 AND eventdepth_in - designdepth_in > -1.25 THEN 1
+ELSE 6
+END;
+$BODY$;
+
