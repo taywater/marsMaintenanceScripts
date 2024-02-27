472e215d832956b7159110552157876a18b8b2fb
taywater <taylor.heffernan@phila.gov>
2024-02-27 11:34:17 -0500

    1.1.0 Added documentation page

--- script_dashboard/app.R
+++ script_dashboard/app.R
@@ -25,7 +25,7 @@ library(DBI)
 
 #set default page length for datatables
 options(DT.options = list(pageLength = 15))
-version = '1.0.0'
+version = '1.1.0'
 
 #set db connection
 #using a pool connection so separate connnections are unified
@@ -42,7 +42,68 @@ onStop(function(){
 ui <- navbarPage(paste("Script Dashboard", version), theme = shinytheme("cerulean"),
     
     tabPanel("Condensed View",
-      DTOutput("logs"))
+      DTOutput("logs")),
+    tabPanel("Documentation", value = "readme",
+      titlePanel("MARS Maintenance Script Dashboard v 1.1.0"),
+      column(width = 5,
+        h2("Scripts"),
+        h5("(In order of execution"),
+        
+        h3("DB Backup"),
+        h5("Use pg_dump to archive our database to a folder on \\pwdoows. Use pg_restore to restore a test copy of that archive. Prune backups older than 1 month, saving one monthly copy."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/01_database_backup", "Github Link"),
+        
+        h3("Access DBs"),
+        h5("Crawl the MARS main folder for Access DBs in site folders and register them to be checked for monitoring data."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/02_update_accessdb_tables", "Github Link"),
+        
+        h3("OW Data"),
+        h5("Sequentially query site folder Access DBs for new monitoring data and add it to our database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/03_update_ow_data", "Github Link"),
+        
+        h3("Baro Data"),
+        h5("Check designated baro site folders for new baro files and import them into our database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/04_update_baro_tables", "Github Link"),
+        
+        h3("Rainfall Data"),
+        h5("Query CentralDB for new rainfall data, and process that data into events. Write the events and data to our database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/05_update_rainfall_tables", "Github Link"),
+        
+        h3("Dataconv Scrape"),
+        h5("Import several tables from the DataConv GIS database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/06_update_dataconv_tables", "Github Link"),
+        
+        h3("GreenIT Scrape"),
+        h5("Import several tables from GreenIT."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/07_update_greenit_tables", "Github Link"),
+        
+        h3("CIPIT Scrape"),
+        h5("Import several tables from CIPIT."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/08_update_cipit_tables", "Github Link"),
+        
+        h3("SMP Metadata"),
+        h5("Download some spatial data from DataConv and use it to update each SMP's location, nearest rain gage, and radar grid cell in our database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/09_update_smp_tables", "Github Link"),
+        
+        h3("WIC Data"),
+        h5("Query Cityworks for WIC records, perform GIS operations to locate them relative to SMPs, and update these records in our database."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/10_update_wic_tables", "Github Link"),
+        
+        h3("Plan Review Scrape"),
+        h5("Import several tables from the Plan Review DB."),
+        a(href="https://github.com/taywater/marsMaintenanceScripts/tree/master/11_update_planreview_tables", "Github Link"),
+        
+      ),
+      column(width = 5,
+        h2("Release Notes"),
+        h3("V1.0.0"),
+        h5("Initial release"),
+        
+        h3("v1.1.0"),
+        h5("Added documentation page.")
+      )
+    )
+        
 )
 
 # Server logic
